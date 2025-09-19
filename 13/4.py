fce72359e057023db314bafd41494b7e9271587b
Om-Lathigara <aumlathigara17@gmail.com>
2025-09-19 12:43:44 +0530

    new

--- 13/4.py
+++ 13/4.py
@@ -1,3 +1,6 @@
-with open(r"C:\Users\aumla\Downloads\example.txt") as f1, open(r"C:\Users\aumla\Downloads\ex1.txt") as f2, open("C:\Users\aumla\Downloads\merged.txt", "w") as f3:
-    f3.write(f1.read())
-    f3.write(f2.read())
+with open(r"C:\Users\aumla\Downloads\example.txt", 'r') as f1, \
+     open(r"C:\Users\aumla\Downloads\ex1.txt", 'r') as f2, \
+     open(r"D:\MARWADI\YEAR2\SEM3\PYTHON\13\merged.txt", 'w') as fout:
+    fout.write(f1.read())
+    fout.write("\n")
+    fout.write(f2.read())
